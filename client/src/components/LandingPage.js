7fcf32e56d15ba11dad7c9d8133facc51219bf13
yungdenzel <oyomafia@gmail.com>
2020-01-16 07:22:41 +0100

    middle section

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,58 +1,61 @@
 import React, { Component } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import './LandingPage.css';
-import Owerri from './images/2423.jpg';
-import Lagos from './images/ballroom-decoration6.jpg';
-import Wuse from './images/Z7T7769小.jpg';
-import Ibadan from './images/ballroom-decoration6.jpg';
-import Picture from './images/meeting-hall.jpg';
+import faker from 'faker';
+import Testimonial from './Testimonial';
+import Facility from './Facility';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+
+
+const  responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
 
 export default class LandingPage extends Component {
+
+   
     render() {
         return (
             <div>
                <h5 className="tc bg-light b">Our Facilities</h5>
-                <div className="flex justify-center carousel-of-images ">
-                    <div className="card ma3">
-                        <img src={Owerri} alt="Avatar"/>
-                        <div className="container">
-                            <h6 className="tc ma2">Owerri Ibadan</h6>                             
-                        </div>
-                    </div> 
-                    <div className="card ma3">
-                        <img src={Lagos} alt="Avatar"/>
-                        <div className="container">
-                            <h6 className="tc ma2">Ikeja, Lagos</h6>                             
-                        </div>
-                    </div> 
-                    <div className="card ma3">
-                        <img src={Wuse} alt="Avatar"/>
-                        <div className="container">
-                            <h6 className="tc ma2">Wuse II, Abuja</h6>                             
-                        </div>
-                    </div> 
-                    <div className="card ma3">
-                        <img src={Ibadan} alt="Avatar"/>
-                        <div className="container">
-                            <h6 className="tc ma2">Mokola, Ibadan</h6>                             
-                        </div>
-                    </div>                  
                           
-                </div>
-                            <div className="ma4 bg-light pa2">
-                    <div className="carousel-of-quotes items-center">
-                        <div className="flex justify-center ">
-                            <div className="pa4">
-                        <p className="gray f4"> Beautiful Images to use for all social and corporate functions.
-                        <br/> Massive car park space which is different from the usual.
-                        </p>
-                        <p className="content-center  f6 pt3">Kolade Cole <br/> Event Planner | Sarah 'O' events</p>
-                        </div>
-                        <img className="fr  round-image" src={Picture} alt="picture"/>
-                        </div>
-                        
-                       
-                    </div>
+                <Carousel
+                showDots={true} 
+                responsive={responsive}>
+                           <Facility Location = "Ikeja, Lagos" Image ={faker.image.city()}/>                
+                           <Facility Location = "Owerri" Image ={faker.image.city()}/>                                             
+                           <Facility Location = "Wuse II, Abuja"  Image ={faker.image.city()}/>                                              
+                            <Facility Location = "Mokola, Ibadan"  Image ={faker.image.city()}/>     
+                            <Facility Location = "Lokoja" Image ={faker.image.city()}/>                                             
+                           <Facility Location = "Kano"  Image ={faker.image.city()}/>                                              
+                            <Facility Location = "Ijebu, Osun"  Image ={faker.image.city()}/>                     
+                 
+                
+                </Carousel>;           
+                
+                    <div className=" pa2 bg-light-gray">
+                            <Testimonial 
+                            quote="Beautiful Images to use for all social and corporate functions.
+                            Massive car park space which is different from the usual."
+                            author="Kolade Cole " job="Event Planner | Sarah 'O' events"
+                            avatar={faker.image.avatar()}
+                           />
                     </div>
 
             </div>
